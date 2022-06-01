d38453296834e6f546cdb924db04e76e6d35a8bc
Marcus Munoz <munoz.marcus05@gmail.com>
2022-06-01 14:02:42 -0500

    template cards for movie info

--- movie_app.js
+++ movie_app.js
@@ -1,4 +1,22 @@
+// Loading screen
+fetch('https://tinted-good-pufferfish.glitch.me/movies')
+    .then(function (res) {
+        console.log(res)
+        return res.json();
+    }).then(function (data){
+    console.log(data)
+    $("#loading-image").addClass('hide')
 
+    console.log(data[23].title)
+
+    // const info = data.map(function (x){
+    //     $(".card-header").append(data[23].title)
+    // })
+
+    data.forEach(function (movie){
+            $(".card-header").append(data[23].title)
+    })
+})
 
 
 
