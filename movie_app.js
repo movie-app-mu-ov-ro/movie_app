313cbb9b454759d9cb2fe5e85b597460aaf6a675
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-06-02 16:54:47 -0500

    refactored movie_app

--- movie_app.js
+++ movie_app.js
@@ -1,5 +1,6 @@
 // Loading screen
-fetch('https://tinted-good-pufferfish.glitch.me/movies')
+const url = 'https://tinted-good-pufferfish.glitch.me/movies';
+fetch(url)
     .then(function (res) {
         console.log(res)
         return res.json();
@@ -22,19 +23,37 @@ fetch('https://tinted-good-pufferfish.glitch.me/movies')
 //     })
 // })
 
+
+
+
     const htmlText = data.map(function(o){
+
         return`
          <div class="card">
         <div class="card-header">Title: ${o.title}</div>
         <div class="card-body"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH_s11214cj0Z8H_o9SkPsIArI8tsF4SIQZg&usqp=CAU"></div>
         <div class="card-footer">Rating: ${o.rating}</div>
         <div class="card-footer">ID: ${o.id}</div>
-        <button type="button" class="btn btn-primary"><span class="bi bi-trash"></span></button></div>
+        <button type="button" class="btn btn-primary" onclick="myFunction"><span class="bi bi-trash"></span></button></div>
 <br>
+
 `;
     });
 
     $('#movies').append(htmlText)
+
+    function deleteMovie(id) {
+        const removeMovie = fetch(`${url}/${id}`, {
+
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .catch(error => console.error(error))
+    }
+
 
 //         `<div class="card">
 //     <div class="container">
@@ -52,18 +71,50 @@ fetch('https://tinted-good-pufferfish.glitch.me/movies')
     //   </div>
     //   `;
 
-    const url = 'https://tinted-good-pufferfish.glitch.me/movies/{287}';
-    const options = {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
 
-        body: JSON.stringify(data),
-    };
-    fetch(url, options)
-        .then( response => console.log(response) ) /* review was created successfully */
-        .catch( error => console.error(error) ); /* handle errors */
+
+    let title = document.getElementById('addMovie')
+    let rating = document.getElementById('addRating')
+    const btn1 = document.getElementById("btn1")
+
+    function createMovie() {
+
+        let movie = {
+            'title': title.value,
+            'rating': rating.value
+        }
+
+        const url = 'https://tinted-good-pufferfish.glitch.me/movies';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movie),
+        };
+        fetch(url, options)
+            .then( response => console.log(response) ) /* review was created successfully */
+            .catch( error => console.error(error) ); /* handle errors */
+
+
+
+        console.log(movie)
+    }
+
+    btn1.addEventListener("click", createMovie)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
 
