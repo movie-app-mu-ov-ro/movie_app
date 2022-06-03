7aa9f7f960e5b07f53fe26f9389cf818447daabd
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-06-03 12:25:16 -0500

    add changes to movie_app

--- movie_app.js
+++ movie_app.js
@@ -7,36 +7,17 @@ fetch(url)
     }).then(function (data){
     console.log(data)
     $("#loading-image").addClass('hide')
-
     console.log(data[0].title)
 
-    // const info = data.map(function (x){
-    //     $(".card-header").append(data[23].title)
-    // })
-
-//     data.forEach(function (movie, index){
-//         if (index < 1) {
-//             $(".card-header").append(data[0].title)
-//             $(".card-body").append(data[0].rating)
-//             $(".card-footer").append(data[0].id)
-//         }
-//     })
-// })
-
-
-
-
     const htmlText = data.map(function(o){
-
         return`
          <div class="card">
         <div class="card-header">Title: ${o.title}</div>
         <div class="card-body"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH_s11214cj0Z8H_o9SkPsIArI8tsF4SIQZg&usqp=CAU"></div>
         <div class="card-footer">Rating: ${o.rating}</div>
         <div class="card-footer">ID: ${o.id}</div>
-        <button type="button" class="btn btn-primary" onclick="myFunction"><span class="bi bi-trash"></span></button></div>
+        <button type="button" class="btn btn-primary" onclick="deleteMovie"><span class="bi bi-trash"></span></button></div>
 <br>
-
 `;
     });
 
@@ -53,25 +34,7 @@ fetch(url)
             .then((response) => response.json())
             .catch(error => console.error(error))
     }
-
-
-//         `<div class="card">
-//     <div class="container">
-//         <div class="card-header">Title:${o.title}</div>
-//         <div class="card-body">Rating:${o.rating}</div>
-//         <div class="card-footer">ID:${o.id}</div>
-//     </div>
-// </div>`
-
-    // return `
-    //   <div class="div-conatiner">
-    //   <p class="p-name"> Title: ${o.title}</p>
-    //   <p class="p-loc"> Rating: ${o.rating}</p>
-    //   <p class="p-desc"> ID: ${o.id}</p><br>
-    //   </div>
-    //   `;
-
-
+//  Create Movie Form
 
     let title = document.getElementById('addMovie')
     let rating = document.getElementById('addRating')
@@ -102,19 +65,6 @@ fetch(url)
     }
 
     btn1.addEventListener("click", createMovie)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
 
